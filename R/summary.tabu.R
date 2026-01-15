1a137d6898495203dc1eda9357e9d963f581280a
kat <kat@mu.ie>
2026-01-15 12:15:29 +0000

    adding roxygen

--- R/summary.tabu.R
+++ R/summary.tabu.R
@@ -1,32 +1,56 @@
+#' R Based Tabu Search Summary Function
+#' Summarizes the results of a tabu search optimization run.
+#'
+#' @param object a tabu object.
+#' @param verbose if true, the optimal configuration(s) will be printed.
+#' @param ... other options (ignored).
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' # A simple example
+#' evaluateSimple <- function(th)return(1)
+#' result <- tabuSearch(size = 20, iters = 100, objFunc = evaluateSimple)
+#' summary(result)
+#' summary(result, verbose = TRUE)
+#'
+#'
+#'
+#'
+
+#' @describeIn summary  summary method
+#' @export
+#'
 summary.tabu <-
 function (object, verbose = FALSE, ...)
 {
     tabuObject <- object
-     
+
     nVars <- rowSums(tabuObject$configKeep)
     nSelect <- colSums(tabuObject$configKeep)
     uniqueConfig <- dim(unique(tabuObject$configKeep))[1]
-    
-    
-    output <- paste("Tabu Settings", "\n", 
-    "  Type                                       = ", tabuObject$type, "\n", 
-    "  No of algorithm repeats                    = ", tabuObject$repeatAll, "\n",    
+
+
+    output <- paste("Tabu Settings", "\n",
+    "  Type                                       = ", tabuObject$type, "\n",
+    "  No of algorithm repeats                    = ", tabuObject$repeatAll, "\n",
     "  No of iterations at each prelim search     = ", tabuObject$iters, "\n",
     "  Total no of iterations                     = ", length(nVars), "\n",
-    "  No of unique best configurations           = ", uniqueConfig, "\n", 
-    "  Tabu list size                             = ", tabuObject$listSize, "\n", 
-    "  Configuration length                       = ", length(nSelect), "\n", 
+    "  No of unique best configurations           = ", uniqueConfig, "\n",
+    "  Tabu list size                             = ", tabuObject$listSize, "\n",
+    "  Configuration length                       = ", length(nSelect), "\n",
     "  No of neighbours visited at each iteration = ", tabuObject$neigh, "\n",   sep = "")
- 
+
     maxObjFunction <- max(tabuObject$eUtilityKeep)
     optimumNVars <- nVars[which(tabuObject$eUtilityKeep == max(tabuObject$eUtilityKeep))]
     optimumIteration <- which(tabuObject$eUtilityKeep == max(tabuObject$eUtilityKeep))
     optimumConfig <- tabuObject$configKeep[optimumIteration, ]
-  
-    optionPart <- paste("Results:", "\n", 
-    "  Highest value of objective fn    = ", round(maxObjFunction, 5), "\n", 
+
+    optionPart <- paste("Results:", "\n",
+    "  Highest value of objective fn    = ", round(maxObjFunction, 5), "\n",
     "  Occurs # of times                = ", length(optimumNVars), "\n",
-    "  Optimum number of variables      = ", deparse(optimumNVars),  "\n",  sep = "")  
+    "  Optimum number of variables      = ", deparse(optimumNVars),  "\n",  sep = "")
 
     cat(output)
     cat(optionPart)
--- R/tabuSearch.R
+++ R/tabuSearch.R
@@ -1,3 +1,19 @@
+#' Title
+#'
+#' @param size
+#' @param iters
+#' @param objFunc
+#' @param config
+#' @param neigh
+#' @param listSize
+#' @param nRestarts
+#' @param repeatAll
+#' @param verbose
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 tabuSearch <-
 function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, listSize = 9, nRestarts = 10,
  repeatAll = 1, verbose = FALSE){
@@ -17,7 +33,7 @@ function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, li
         stop("A evaluation function must be provided. See the objFunc parameter.")
     }
     if (is.null(config)) {
-        config <- matrix(0, 1, size) 
+        config <- matrix(0, 1, size)
         config[sample(1:size, sample(1:size, 1))] <- 1
     }
     else if (size != length(config)){
@@ -30,54 +46,54 @@ function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, li
     iter <- 1
     configKeep <- matrix(, repeatAll * iters * (nRestarts + 3), size)
     eUtilityKeep <- vector(, repeatAll * iters * (nRestarts + 3))
-    
+
     for(j in 1:repeatAll){
-        
+
         if (j > 1) {
-            config <- matrix(0, 1, size) 
+            config <- matrix(0, 1, size)
             config[sample(1:size, sample(1:size, 1))] <- 1
         }
-        
+
         tabuList <- matrix(0, 1, size)
         listOrder <- matrix(0, 1, listSize) #remembers order of tabu moves
 
         eUtility <- objFunc(config)
         aspiration <- eUtility    #highest utility found so far
-        
-    
+
+
         preliminarySearch<- function(){
-    
+
             configKeep[iter, ] <- config
             eUtilityKeep[iter] <- eUtility
             iter <- iter + 1
-    
+
             for (i in 2:iters){
-                neighboursEUtility <- matrix(0, 1, size)  
+                neighboursEUtility <- matrix(0, 1, size)
                 configTemp <- t(matrix(config, size, neigh))
                 randomNeighbours <- sample(size, neigh) #pick random neighbours
                 diag(configTemp[, randomNeighbours]) <- abs(diag(configTemp[, randomNeighbours]) - 1)#flip
-                neighboursEUtility[randomNeighbours] <- apply(configTemp, 1, objFunc) 
+                neighboursEUtility[randomNeighbours] <- apply(configTemp, 1, objFunc)
                 maxNontaboo <- max(neighboursEUtility[tabuList == 0])
                 maxTaboo <- max(neighboursEUtility[tabuList == 1], -Inf)
-      
-                #find new move                
-                move <- ifelse(maxTaboo > maxNontaboo & maxTaboo > aspiration, 
-                ifelse(length(which(neighboursEUtility == maxTaboo)) == 1, 
-                       which(neighboursEUtility == maxTaboo), sample(which(neighboursEUtility == maxTaboo), 1)),  
-                ifelse(length(which(neighboursEUtility == maxNontaboo & tabuList == 0)) == 1, 
+
+                #find new move
+                move <- ifelse(maxTaboo > maxNontaboo & maxTaboo > aspiration,
+                ifelse(length(which(neighboursEUtility == maxTaboo)) == 1,
+                       which(neighboursEUtility == maxTaboo), sample(which(neighboursEUtility == maxTaboo), 1)),
+                ifelse(length(which(neighboursEUtility == maxNontaboo & tabuList == 0)) == 1,
                        which(neighboursEUtility == maxNontaboo & tabuList == 0), sample(which(neighboursEUtility == maxNontaboo & tabuList == 0), 1)))
-          
-                #if new utility is lower than old, add move to tabu list, else adjust aspiration, if necessary               
+
+                #if new utility is lower than old, add move to tabu list, else adjust aspiration, if necessary
                 if (eUtility >= neighboursEUtility[move]){
                     tabuList[move] <- 1
                     if(sum(tabuList) > listSize){ #if tabu list is full
                         tabuList[listOrder[1]] <- 0
                         listOrder[1:listSize] <- c(listOrder[2:listSize], 0)
-                        } 
+                        }
                     listOrder[min(which(listOrder == 0))] <- move
                     }
                 else if(neighboursEUtility[move] > aspiration) aspiration <- neighboursEUtility[move]
-    
+
                 #make the new move
                 eUtility <- neighboursEUtility[move]
                 config[move] <- abs(config[move]-1)
@@ -85,12 +101,12 @@ function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, li
                 eUtilityKeep[iter] <- eUtility
                 iter <- iter + 1
             }
-    
+
             result = list(aspiration = aspiration,  configKeep = configKeep, eUtilityKeep = eUtilityKeep, iter = iter)
             return(result)
         }
-        
-    
+
+
         #PRELIMINARY SEARCH
         if (verbose) cat("Preliminary search stage...\n")
         result <- preliminarySearch()
@@ -98,7 +114,7 @@ function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, li
         configKeep <- result$configKeep
         eUtilityKeep <- result$eUtilityKeep
         iter <- result$iter
-    
+
         #INTENSIFICATION
     	  tempo <- 0
     	  restarts <- 0
@@ -107,34 +123,34 @@ function(size = 10, iters = 100, objFunc = NULL, config = NULL, neigh = size, li
             eUtility <- max(eUtilityKeep)
             tempo <- aspiration
             config <- configKeep[max(which(eUtilityKeep == max(eUtilityKeep))), ]
-            result <- preliminarySearch()  
+            result <- preliminarySearch()
             aspiration <- result$aspiration
             configKeep <- result$configKeep
             eUtilityKeep <- result$eUtilityKeep
             iter <- result$iter
             restarts <- restarts + 1
         }
-    
-           	  
+
+
         #DIVERSIFICATION
         if (verbose) cat("Diversification stage...\n")
-        config <- matrix(0, 1, size) 
+        config <- matrix(0, 1, size)
         config[sample(1:size, sample(1:size, 1))] <- 1
         eUtility <- objFunc(config)
-    
-        frequent <- apply(configKeep, 2, function(x)sum(diff(x) != 0))   #create new tabu list from most frequent moves      
+
+        frequent <- apply(configKeep, 2, function(x)sum(diff(x) != 0))   #create new tabu list from most frequent moves
         tabuList <- as.numeric(rank(frequent, ties.method =  "random") > (size - listSize))
         listOrder <- sample(which(rank(frequent, ties.method =  "random") > (size - listSize)), listSize)
-    
+
         result <- preliminarySearch()
         iter <- result$iter
         configKeep <- result$configKeep
         eUtilityKeep <- result$eUtilityKeep
     }
 
-    endResult <- list(type = "binary configuration", configKeep = configKeep[1:(iter - 1), ], 
-    eUtilityKeep = eUtilityKeep[1:(iter - 1)], iters = iters, neigh = neigh, 
+    endResult <- list(type = "binary configuration", configKeep = configKeep[1:(iter - 1), ],
+    eUtilityKeep = eUtilityKeep[1:(iter - 1)], iters = iters, neigh = neigh,
     listSize = listSize,  repeatAll = repeatAll)
-    class(endResult) = "tabu"  
+    class(endResult) = "tabu"
     return(endResult)
 }
