81c684a53eb1834a90a7bbbdfa81472e0c150a72
kat <kat@mu.ie>
2026-01-15 13:30:55 +0000

    namespace automatic generation

---
+++ R/tabuSearch-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics image
+#' @importFrom graphics layout
+#' @importFrom graphics par
+#' @importFrom grDevices gray
+## usethis namespace: end
+NULL
